0767bdf07cf51144cda2ab19a13680d8b9bfa771
a.arkhipov <a.arkhipov@viseven.com>
2021-12-06 23:29:22 +0200

    Finished push notifications

--- App.tsx
+++ App.tsx
@@ -4,17 +4,19 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+import messaging from '@react-native-firebase/messaging';
 import { NavigationContainer, DefaultTheme as NavigationTheme } from '@react-navigation/native';
 import { OverflowMenuProvider } from 'react-navigation-header-buttons';
-import { Provider as StoreProvider } from 'react-redux';
 
-import { store } from './src/store';
 import { AppNavigator } from './src/navigation/AppNavigation';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import './src/localization/i18n';
+
+import { IRootState } from './src/store/index';
 
 const MyTheme = {
   ...NavigationTheme,
@@ -34,16 +36,64 @@ const theme = {
 };
 
 const App = () => {
+  const notifications = useSelector((state: IRootState) => state.userSettingsReducer.notifications);
+
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if (notifications) {
+      (async () => {
+        requestUserPermission();
+      })();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (notifications) {
+      messaging().onNotificationOpenedApp(remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+      });
+
+      // Check whether an initial notification is available
+      messaging()
+        .getInitialNotification()
+        .then(remoteMessage => {
+          if (remoteMessage) {
+            console.log(
+              'Notification caused app to open from quit state:',
+              remoteMessage.notification,
+            );
+          }
+        });
+
+      messaging().onMessage(async remoteMessage => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+      });
+    }
+  }, []);
+
   return (
-    <StoreProvider store={store}>
-      <PaperProvider theme={theme}>
-        <NavigationContainer theme={MyTheme}>
-          <OverflowMenuProvider>
-            <AppNavigator />
-          </OverflowMenuProvider>
-        </NavigationContainer>
-      </PaperProvider>
-    </StoreProvider>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={MyTheme}>
+        <OverflowMenuProvider>
+          <AppNavigator />
+        </OverflowMenuProvider>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -30,8 +30,6 @@ export const AppNavigator: React.FC = () => {
   const [user, setUser] = useState();
   const [initializing, setInitializing] = useState(true);
 
-  console.log('sub', user);
-
   function onAuthStateChanged(authData) {
     setUser(authData);
     if (initializing) {
@@ -76,10 +74,8 @@ export const AppNavigator: React.FC = () => {
 
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    
-    
     return subscriber; // unsubscribe on unmount
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   });
 
   if (initializing) {
